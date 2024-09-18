63035ba4ccf65be8a165ad4a031792405f7f8cb7
tfelde001 <36023792+tfelde001@users.noreply.github.com>
2024-09-18 18:50:05 -0400

    first

--- app_test.py
+++ app_test.py
@@ -1,56 +1,21 @@
-import datetime
-from unittest.mock import patch
-from streamlit.testing.v1 import AppTest
-from openai.types.chat import ChatCompletionMessage
-from openai.types.chat.chat_completion import ChatCompletion, Choice
+//@version=5
+strategy("Simulated TICK Reversal Strategy", overlay=true, default_qty_type=strategy.percent_of_equity, default_qty_value=1, initial_capital=10000)
 
+// Simulated TICK Index
+advancing = request.security("NYSE:ADV", "1", close)
+declining = request.security("NYSE:DEC", "1", close)
+tick = advancing - declining
 
-# See https://github.com/openai/openai-python/issues/715#issuecomment-1809203346
-def create_chat_completion(response: str, role: str = "assistant") -> ChatCompletion:
-    return ChatCompletion(
-        id="foo",
-        model="gpt-3.5-turbo",
-        object="chat.completion",
-        choices=[
-            Choice(
-                finish_reason="stop",
-                index=0,
-                message=ChatCompletionMessage(
-                    content=response,
-                    role=role,
-                ),
-            )
-        ],
-        created=int(datetime.datetime.now().timestamp()),
-    )
+// TICK Levels
+tick_short_entry = 1000
+tick_long_entry = -1000
 
+// Entry Conditions
+short_condition = tick >= tick_short_entry
+long_condition = tick <= tick_long_entry
 
-@patch("openai.resources.chat.Completions.create")
-def test_Chatbot(openai_create):
-    at = AppTest.from_file("Chatbot.py").run()
-    assert not at.exception
-    at.chat_input[0].set_value("Do you know any jokes?").run()
-    assert at.info[0].value == "Please add your OpenAI API key to continue."
+// Take Profit and Stop Loss
+take_profit_percent = 0.50 / 100
+stop_loss_percent = 0.25 / 100
 
-    JOKE = "Why did the chicken cross the road? To get to the other side."
-    openai_create.return_value = create_chat_completion(JOKE)
-    at.text_input(key="chatbot_api_key").set_value("sk-...")
-    at.chat_input[0].set_value("Do you know any jokes?").run()
-    print(at)
-    assert at.chat_message[1].markdown[0].value == "Do you know any jokes?"
-    assert at.chat_message[2].markdown[0].value == JOKE
-    assert at.chat_message[2].avatar == "assistant"
-    assert not at.exception
-
-
-@patch("langchain.llms.OpenAI.__call__")
-def test_Langchain_Quickstart(langchain_llm):
-    at = AppTest.from_file("pages/3_Langchain_Quickstart.py").run()
-    assert at.info[0].value == "Please add your OpenAI API key to continue."
-
-    RESPONSE = "1. The best way to learn how to code is by practicing..."
-    langchain_llm.return_value = RESPONSE
-    at.sidebar.text_input[0].set_value("sk-...")
-    at.button[0].set_value(True).run()
-    print(at)
-    assert at.info[0].value == RESPONSE
+//
